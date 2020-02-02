cc9b7b6b205d62ec2be1e1361c56f7b0d9844643
Tommy Vestermark <tovsurf@vestermark.dk>
2020-02-02 21:40:14 +0100

    SerialThread: Make periodic..

--- src/SerialThread.hpp
+++ src/SerialThread.hpp
@@ -1,32 +1,37 @@
 #include "TF/Log.h"
 #include "TF/Serial.h"
 #include "TF/Thread.h"
+#include "TF/Time.h"
 #include <string.h>
 
 class SerialThread : public TF::Thread {
     void run() {
-        TF::Log::debug("SerialThread");
-
-        serial.open();
-        serial.flushRxBuffer();
         char tx_buffer[32];
         char rx_buffer[32];
         char str[32];
-
         int n=0;
 
+        TF::Log::debug("SerialThread");
+
+        serial.open("UART_2", 115200);
+        serial.flushRxBuffer();
+
+        run_period.set_timeout_ms(10);  // 100Hz
+        run_period.reset();
+
         while(1) {
-            TF::Thread::sleep_ms(500);
-            // TX
-            snprintf(tx_buffer, sizeof(tx_buffer), "Testing %2i", n++);
-            TF::Log::debug("Sending: %s", tx_buffer);
-            serial.write((uint8_t*) tx_buffer, strlen(tx_buffer));
-            TF::Thread::sleep_ms(10);
+            // Periodic loop
+            statistics.maxRunTime = MAX(statistics.maxRunTime, run_period.get_elapsed_ms());
+            while(run_period.get_remaining_ms() > 0) {
+                TF::Thread::sleep_ms(run_period.get_remaining_ms());
+            }
+            run_period.reset();
+
             // RX
             int bytes = serial.getRxBytes();
             if(bytes) {
                 serial.read((uint8_t *)rx_buffer, bytes);
-                rx_buffer[bytes] = 0;
+                rx_buffer[bytes] = 0;   // safe string termination..
 
                 // Bin2Hex
                 for (int n=0; n<bytes; ++n) {
@@ -34,12 +39,25 @@ class SerialThread : public TF::Thread {
                 }
                 TF::Log::debug("Received: %s, [%u] %s", rx_buffer, bytes, str);
             }
+
+            // TX
+            snprintf(tx_buffer, sizeof(tx_buffer), "Testing %2i", n++);
+            TF::Log::debug("Sending: %s", tx_buffer);
+            serial.write((uint8_t*) tx_buffer, strlen(tx_buffer));
         }
-//			queue.send((uint8_t*)"MSG1", 256);
-//			queue.send((uint8_t*)"MSG2", 8);
+        // Test queue
+//      queue.send((uint8_t*)"MSG1", 256);
+//      queue.send((uint8_t*)"MSG2", 8);
     }
 
     TF::Serial serial;
+
+    TF::Time run_period;
+    class Statistics {
+    public:
+        TF::TimeStamp   maxRunTime;
+
+    } statistics;
 
 #if 0
 //#ifdef __linux__
